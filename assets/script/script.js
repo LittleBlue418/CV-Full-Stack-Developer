61606b084f03c8dca1739bf0fab1ef3f61b3d59d
LittleBlue418 <hollyathomas88@gmail.com>
2019-08-08 16:44:41 +0200

    Skills progress circles code added + jQuery + JavaScript

---
+++ assets/script/script.js
@@ -0,0 +1,30 @@
+// This code is from https://bootstrapious.com/snippets 
+
+$(function() {
+
+    $(".progress").each(function() {
+  
+      var value = $(this).attr('data-value');
+      var left = $(this).find('.progress-left .progress-bar');
+      var right = $(this).find('.progress-right .progress-bar');
+  
+      if (value > 0) {
+        if (value <= 50) {
+          right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+        } else {
+          right.css('transform', 'rotate(180deg)')
+          left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+        }
+      }
+  
+    })
+  
+    function percentageToDegrees(percentage) {
+  
+      return percentage / 100 * 360
+  
+    }
+  
+  });
+
+  // end of code from https://bootstrapious.com/snippets 
