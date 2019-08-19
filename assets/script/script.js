178ef7171130997fb2e8e3d7d2af490b7ecbc778
LittleBlue418 <hollyathomas88@gmail.com>
2019-08-19 21:27:21 +0200

    Added scroll to top of section when inner section is expanded

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,13 +1,14 @@
-// This code is from https://bootstrapious.com/snippets 
+
 
 $(function() {
 
+// This code is from https://bootstrapious.com/snippets
     $(".progress").each(function() {
-  
+
       var value = $(this).attr('data-value');
       var left = $(this).find('.progress-left .progress-bar');
       var right = $(this).find('.progress-right .progress-bar');
-  
+
       if (value > 0) {
         if (value <= 50) {
           right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
@@ -16,19 +17,37 @@ $(function() {
           left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
         }
       }
-  
-    })
-  
-    function percentageToDegrees(percentage) {
-  
-      return percentage / 100 * 360
-  
-    }
 
-    $('#aboutMeSection').on('shown.bs.collapse', function() {
-        console.log("Scrolling!");
     })
-  
+
+    function percentageToDegrees(percentage) {
+
+      return percentage / 100 * 360
+
+    }
+    // end of code from https://bootstrapious.com/snippets
+
+
+    //target a section | when this happes: | the event 'shown' happens on the boostrap class 'collpse' | call this function and gives it the event (the list of stuff that happens)
+    $('.inner-section').on('show.bs.collapse', function(event) {
+
+        //create a variable | put in the target for the event (the section) | call the method to find the first parent element (with this matching name)
+        var section = $(event.target).closest('.page-section');
+
+        //create another variable | set it to the parent element we just found
+        var offset = section.offset();
+
+        //check we have targeted the correct element and it's giving us the location
+        console.log(section);
+        console.log(offset);
+
+        //create a new variable that contains only the value top
+        var properties = {scrollTop:offset.top};
+        console.log(properties);
+
+        //create a collection containing the html page and the body | call the method 'animate' |
+        $('html, body').animate(properties,1000);
+    })
+
   });
 
-  // end of code from https://bootstrapious.com/snippets 
